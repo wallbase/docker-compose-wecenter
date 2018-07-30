e23f89be1805de9908b45d2719b69bf332c2373b
wangkun23 <wang.kun@jinyinwu.com>
2018-07-30 13:03:56 +0800

    INIT wecenter

---
+++ WeCenter_3-2-1/tmp/plugins_model.php
@@ -0,0 +1,1 @@
+a:2:{s:26:"aws_offical_external_class";s:87:"/Users/huangzhijie/Desktop/source/plugins/aws_offical_external/aws_offical_external.php";s:18:"aws_external_class";s:71:"/Users/huangzhijie/Desktop/source/plugins/aws_external/aws_external.php";}
